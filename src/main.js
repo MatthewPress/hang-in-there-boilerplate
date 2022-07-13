c3c96da80ee44fd254334648aac353eae8b28fdf
Shaya Golafshani <s.golaff@gmail.com>
2022-07-12 19:52:51 -0600

    Push up the edits that I made

--- src/main.js
+++ src/main.js
@@ -104,6 +104,7 @@ var currentPoster;
 // event listeners go here 👇
 
 // functions and event handlers go here 👇
+
 // (we've provided one for you to get you started)!
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
